1b71968cc9f4c113c3aee0983604709c4e4fc7ec
Martynas Šiškauskas <martynassiskauskas@gmail.com>
2022-01-30 11:31:28 +0200

    Voice over on interactables

---
+++ Assets/Scripts/InteractableVoice.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Interactable))]
+public class InteractableVoice : MonoBehaviour
+{
+    [SerializeField] bool randomOrder;
+    [SerializeField] bool playOnce;
+
+    [SerializeField] List<VoiceOver> VO;
+
+    List<VoiceOver> Unplayed;
+
+    private void Awake()
+    {
+        GetComponent<Interactable>().onInteracted.AddListener(Interact);
+        Unplayed = new List<VoiceOver>(VO);
+    }
+
+    private void Interact()
+    {
+        if(Unplayed.Count <= 0)
+        {
+            if(playOnce)
+            {
+                return;
+            }
+            else
+            {
+                Unplayed = new List<VoiceOver>(VO);
+            }
+        }
+
+        VoiceOver vo = null;
+        if(randomOrder)
+        {
+            vo = Unplayed.PickRandom();
+            Unplayed.Remove(vo);
+        }
+        else
+        {
+            vo = Unplayed[0];
+            Unplayed.RemoveAt(0);
+        }
+
+        SubtitleManager.instance.SetSubtitle(vo);
+    }
+}
